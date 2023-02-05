6d57a4ee80bb90322a07ce0589daadaec280f781
emaldonadogamedev <em_gamedev@outlook.com>
2023-02-04 16:44:51 -0800

    fixing wait enemy beetle wave data

--- Assets/Scripts/ScriptableObjects/TimeBeetleEnemyPair.cs
+++ Assets/Scripts/ScriptableObjects/TimeBeetleEnemyPair.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [Serializable]
 public struct TimeBeetleEnemyPair
 {
-    [Range(0f, 10f)]
+    [Min(0f)]
     public float m_timeToWaitBeforeSpawning;
 
     public GameObject m_enemyBeetleToSpawn;
